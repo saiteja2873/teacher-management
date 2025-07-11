3e9b9fb681a06f386aa55880ed6499269292cf97
saiteja2873 <pasamsaiteja2020@gmail.com>
2025-07-11 17:45:04 +0530

    Removed useState in Logout page

--- frontend/src/app/logout/page.tsx
+++ frontend/src/app/logout/page.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
